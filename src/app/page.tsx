46043948d1cccd339424b2d4e2a7d1c167c083f5
Kevin Nanvou <nanvoukevinstyve@gmail.com>
2025-06-08 18:21:21 +0100

    fin site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,48 +58,69 @@ export default function Home() {
           <div className='flex items-center justify-center'>
             <Image src="/images/logo.png" alt="Mink's et Bad Nova sur scène" width={100} height={100} />
           </div>
-          <div className="py-4 sm:py-12">
+          <div className="mx-auto mt-7 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+              <div className="py-4 sm:py-12">
 
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-              <div className='flex-col items-center justify-center text-center'>
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                  <div className='flex-col items-center justify-center text-center'>
 
-                <h2 className="text-base font-semibold text-yellow-600">
-                  Empire by Krole présente
-                </h2>
-                <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty dark:text-white sm:text-5xl lg:text-balance">
-                    Concert exceptionnel  Mink's & Bad Nova
-                  </p>
-                  <p className="mt-6 text-lg text-gray-400">
-                    Réservez vos billets dès maintenant pour une soirée explosive de musique live à Maroua à 400 places.
-                    Rendez-vous le 12 juin à 18h. Nombre de places limité !
-                  </p>
+                    <h2 className="text-base font-semibold text-yellow-600">
+                      Empire by Krole présente
+                    </h2>
+                    <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty dark:text-white sm:text-5xl lg:text-balance">
+                      Concert exceptionnel  Mink's & Bad Nova
+                    </p>
+                    <p className="mt-6 text-lg text-gray-400">
+                      Réservez vos billets dès maintenant pour une soirée explosive de musique live à Maroua à 400 places.
+                      Rendez-vous le 12 juin à 18h. Nombre de places limité !
+                    </p>
+                  </div>
+                </div>
+
+                <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                  <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                    {features.map((feature) => (
+                      <div key={feature.name} className="relative pl-16">
+                        <dt className="text-base font-semibold">
+                          <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-yellow-600">
+                            <feature.icon aria-hidden="true" className="size-6  text-black" />
+                          </div>
+                          {feature.name}
+                        </dt>
+                        <dd className="mt-2 text-base text-gray-400">
+                          {feature.description}
+                        </dd>
+                      </div>
+                    ))}
+                  </dl>
+                </div>
+
+                <div className="flex items-center justify-center mt-10">
+                  <Button variant="outline" className='w-80 h-15'><Link href="/ticket" className='font-stretch-200% font-extrabold sm:text-3xl lg:text-balance'>Achète ton ticket</Link></Button>
+                </div>
+              </div>
+              <div className="py-4 sm:py-12 gap-6">
+                    <div className='flex items-center justify-between'>
+                      Mon pétit halla Madrid
+                <Image src="/images/mic.png" alt="Mink's et Bad Nova sur scène" width={200} height={300} />
+                
+              </div>
+              <div className='flex items-center justify-between'>
+
+                <Image src="/images/mink.jpg" alt="Mink's et Bad Nova sur scène" width={200} height={300} />
+                Le gard là est laid
+              </div>
+              <div className='flex items-center justify-between'>
+                Halla Madrid
+                <Image src="/images/madrid.png" alt="Mink's et Bad Nova sur scène" width={200} height={300} />
               </div>
               </div>
-
-              <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-                  {features.map((feature) => (
-                    <div key={feature.name} className="relative pl-16">
-                      <dt className="text-base font-semibold">
-                        <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-yellow-600">
-                          <feature.icon aria-hidden="true" className="size-6  text-black" />
-                        </div>
-                        {feature.name}
-                      </dt>
-                      <dd className="mt-2 text-base text-gray-400">
-                        {feature.description}
-                      </dd>
-                    </div>
-                  ))}
-                </dl>
-              </div>
-
-              <div className="flex items-center justify-center mt-10">
-                <Button variant="outline" className='w-80 h-15'><Link href="/ticket" className='font-stretch-200% font-extrabold sm:text-3xl lg:text-balance'>Achète ton ticket</Link></Button>
-              </div>
-            </div>
+              
+            </dl>
           </div>
         </div>
+      </div>
     </ThemeProvider>
 
   );
